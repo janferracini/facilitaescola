42269df0e283b0d101b753de142d3f152fc4b87e
Jan Ferracini <janferracini@gmail.com>
2020-11-15 16:54:26 -0300

    Tela de cadastro de Admin

--- admin/index.php
+++ admin/index.php
@@ -49,237 +49,260 @@ $base = "$h://$site:$porta/$url"
 
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <script src="js/jquery.inputmask.min.js"></script>
+  <script src="js/bindings/inputmask.binding.js"></script>
+
 </head>
 
 
 <body class="hold-transition sidebar-mini">
   <?php
-    $pagina = $pagina . ".php";
+  $pagina = $pagina . ".php";
 
 
   //VALIDAÇÃO DE LOGADO. SE NÃO TIVER, IR PARA PÁGINA DE LOGIN
   if (!isset($_SESSION["facilita_escola"]["id"])) {
     //incluir login
     include $pagina;
-  } else { 
-  
+  } else {
+
   ?>
 
-  <div class="wrapper">
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-      </ul>
+    <div class="wrapper">
+      <!-- Navbar -->
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+          </li>
+        </ul>
 
-      <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
-        <!-- Botão CONFIG -->
-        <li class="nav-item dropdown">
-          <a class="nav-link"  href="#">
-            <i class="fas fa-key"></i>
-          </a>
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Botão CONFIG -->
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#">
+              <i class="fas fa-key"></i>
+            </a>
 
-        </li>
-        <!-- Botão SAIR -->
-        <li class="nav-item dropdown">
-          <a class="nav-link"  href="#" data-toggle="modal" data-target="#logoutModal">
-            <i class="fas fa-sign-out-alt"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.navbar -->
+          </li>
+          <!-- Botão SAIR -->
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
+              <i class="fas fa-sign-out-alt"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.navbar -->
 
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-orange elevation-4">
-      <!-- Brand Logo -->
-      <a href="index.php" class="brand-link">
-        <img src="img/facilitaescola-logo.png" alt="Facilita Escola Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Facilita Escola</span>
-      </a>
+      <!-- Main Sidebar Container -->
+      <aside class="main-sidebar sidebar-dark-orange elevation-4">
+        <!-- Brand Logo -->
+        <a href="index.php" class="brand-link">
+          <img src="img/facilitaescola-logo.png" alt="Facilita Escola Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <span class="brand-text font-weight-light">Facilita Escola</span>
+        </a>
 
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar nav-legacy nav-flat nav-child-indent flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-item">
-              <a href="#" class="nav-link active">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>Dashboard</p>
-              </a>
-            </li>
+        <!-- Sidebar -->
+        <div class="sidebar">
+          <!-- Sidebar Menu -->
+          <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar nav-legacy nav-flat nav-child-indent flex-column" data-widget="treeview" role="menu" data-accordion="false">
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>Dashboard</p>
+                </a>
+              </li>
 
-            <li class="nav-item has-treeview">
-              <a href="#" class="nav-link">
-                <i class="nav-icon far fa-plus-square"></i>
-                <p>
-                  Cadastros
-                  <i class="fas fa-angle-left right"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="cadastro/aluno" class="nav-link">
-                    <i class="fas fa-user-graduate nav-icon"></i>
-                    <p>Alunos</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="professor" class="nav-link">
-                    <i class="fas fa-apple-alt nav-icon"></i>
-                    <p>Professores</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="cadastro/admin" class="nav-link">
-                    <i class="fas fa-laptop-code nav-icon"></i>
-                    <p>Equipe Administrativa</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="cadastro/disciplina" class="nav-link">
-                    <i class="fas fa-book-open nav-icon"></i>
-                    <p>Disciplina</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="cadastro/turma" class="nav-link">
-                    <i class="fas fa-chalkboard nav-icon"></i>
-                    <p>Turmas</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon far fa-plus-square"></i>
+                  <p>
+                    Cadastros
+                    <i class="fas fa-angle-left right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="cadastro/aluno" class="nav-link">
+                      <i class="fas fa-user-graduate nav-icon"></i>
+                      <p>Alunos</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="professor" class="nav-link">
+                      <i class="fas fa-apple-alt nav-icon"></i>
+                      <p>Professores</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="cadastro/admin" class="nav-link">
+                      <i class="fas fa-laptop-code nav-icon"></i>
+                      <p>Equipe Administrativa</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="cadastro/disciplina" class="nav-link">
+                      <i class="fas fa-book-open nav-icon"></i>
+                      <p>Disciplina</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="cadastro/turma" class="nav-link">
+                      <i class="fas fa-chalkboard nav-icon"></i>
+                      <p>Turmas</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
 
-            <li class="nav-item">
-              <a href="pages/calendar.html" class="nav-link">
-                <i class="nav-icon fas fa-pencil-alt"></i>
-                <p>
-                  Atividades
-                </p>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon fas fa-pencil-alt"></i>
+                  <p>
+                    Atividades
+                  </p>
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a href="pages/calendar.html" class="nav-link">
-                <i class="nav-icon fas fa-calendar-alt"></i>
-                <p>
-                  Eventos
-                </p>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon fas fa-calendar-alt"></i>
+                  <p>
+                    Eventos
+                  </p>
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a href="pages/calendar.html" class="nav-link">
-                <i class="nav-icon fas fa-exclamation-triangle"></i>
-                <p>
-                  Recados
-                </p>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon fas fa-exclamation-triangle"></i>
+                  <p>
+                    Recados
+                  </p>
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a href="pages/calendar.html" class="nav-link">
-                <i class="nav-icon fas fa-envelope"></i>
-                <p>
-                  Mensagens
-                </p>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon fas fa-envelope"></i>
+                  <p>
+                    Mensagens
+                  </p>
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a href="pages/calendar.html" class="nav-link">
-                <i class="nav-icon fas fa-folder-open"></i>
-                <p>
-                  Arquivos
-                </p>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon fas fa-folder-open"></i>
+                  <p>
+                    Arquivos
+                  </p>
+                </a>
+              </li>
 
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-
-
-      <!-- Main content -->
-      <div class="content">
-        <div class="container-fluid">
-          <?php
-
-          $pagina = "home.php";
-
-          if (isset($_GET["parametro"])) {
-            //recuperar o parametro
-            $p = trim($_GET["parametro"]);
-            $p = explode("/", $p);
-
-            $pasta      = $p[0];
-            $arquivo    = $p[1];
-
-            $pagina = "$pasta/$arquivo.php";
-            if (isset($p[2]))
-              $id     = $p[2];
-          }
-
-
-          if (file_exists($pagina)) {
-            include $pagina;
-          } else {
-            include "paginas/erro.php";
-          }
-          ?>
-
+            </ul>
+          </nav>
+          <!-- /.sidebar-menu -->
         </div>
-        <!-- /.container-fluid -->
+        <!-- /.sidebar -->
+      </aside>
+
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+
+
+        <!-- Main content -->
+        <div class="content pb-3">
+          <div class="container-fluid">
+            <?php
+
+            $pagina = "home.php";
+
+            if (isset($_GET["parametro"])) {
+              //recuperar o parametro
+              $p = trim($_GET["parametro"]);
+              $p = explode("/", $p);
+
+              $pasta      = $p[0];
+              $arquivo    = $p[1];
+
+              $pagina = "$pasta/$arquivo.php";
+              if (isset($p[2]))
+                $id     = $p[2];
+            }
+
+
+            if (file_exists($pagina)) {
+              include $pagina;
+            } else {
+              include "paginas/erro.php";
+            }
+            ?>
+
+          </div>
+          <!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
       </div>
-      <!-- /.content -->
+      <!-- /.content-wrapper -->
+
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
+      <!-- /.control-sidebar -->
+
+      <!-- Main Footer -->
+      <footer class="main-footer">
+        Copyright &copy; 2020 <strong>Facilita Escola</strong>.
     </div>
-    <!-- /.content-wrapper -->
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-
-    <!-- Main Footer -->
-    <footer class="main-footer">
-      Copyright &copy; 2020 <strong>Facilita Escola</strong>.
-  </div>
-  </footer>
-  </div>
-  <!-- ./wrapper -->
+    </footer>
+    </div>
+    <!-- ./wrapper -->
 
 
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Sair do Sistema?</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">Selecione sair para efetuar o logout do Sistema</div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="sair.php">Logout</a>
-                    </div>
-                </div>
-            </div>
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Sair do Sistema?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Selecione SAIR para efetuar o logout do Sistema</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Voltar</button>
+            <a class="btn btn-primary" href="sair.php">Sair</a>
+          </div>
         </div>
+      </div>
+    </div>
+
+    <!--Modal Desativar Usuário-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Sair do Sistema?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Selecione SAIR para efetuar o logout do Sistema</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Voltar</button>
+            <a class="btn btn-primary" href="sair.php">Sair</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
   <?php
