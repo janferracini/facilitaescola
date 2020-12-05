5918abf6e393af13b037bc32424ee3620ffcb170
clfonte <kamilafonte@gmail.com>
2020-12-05 17:15:42 -0300

    Validação de disciplina repetida

--- admin/salvar/disciplina.php
+++ admin/salvar/disciplina.php
@@ -18,6 +18,27 @@ if ($_POST) {
         echo "<script>alert('Preencha a Série');history.back();</script>";
         exit;
     }
+
+    //verificar se existe disciplina com mesmo nome
+    $sql = "SELECT id
+     FROM disciplina
+     WHERE disciplina = ? AND id <> ?
+     LIMIT 1 ";
+
+    $consulta = $pdo->prepare($sql);
+
+    $consulta->bindParam(1, $disciplina);
+    $consulta->bindParam(2, $id);
+
+    $consulta->execute();
+
+    $dados = $consulta->fetch(PDO::FETCH_OBJ);
+
+    if (!empty($dados->id)) {
+        echo "<script>alert('Disciplina já existente');location.href='listar/disciplina';</script>;";
+        exit;
+    }
+    
     //iniciar uma transação com o DB toda alteração pra baixo, só será feito após o commit
     $pdo->beginTransaction();
 
@@ -28,7 +49,6 @@ if ($_POST) {
                     (:disciplina)";
         $consulta = $pdo->prepare($sql);
         $consulta->bindParam(":disciplina", $disciplina);
-
     } else {
         $sql = "UPDATE disciplina    
                 SET disciplina = :disciplina
@@ -42,11 +62,10 @@ if ($_POST) {
     //executar SQL depois de ver qual ele vai passar
     if ($consulta->execute()) {
 
-            //gravar no DB se tudo estiver OK
-            $pdo->commit();
-            echo "<script>alert('Registro salvo');location.href='listar/disciplina';</script>;";
-            exit;
-        
+        //gravar no DB se tudo estiver OK
+        $pdo->commit();
+        echo "<script>alert('Registro salvo');location.href='listar/disciplina';</script>;";
+        exit;
     }
     echo $consulta->errorInfo()[2];
     exit;
