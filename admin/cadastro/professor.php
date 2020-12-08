12689b42c47cdde4736fa6bebfce24b979ee39ce
Jan Ferracini <48077131+janferracini@users.noreply.github.com>
2020-12-08 11:06:03 -0300

    Atualização SQL

--- admin/cadastro/professor.php
+++ admin/cadastro/professor.php
@@ -202,8 +202,6 @@ if (!empty($id)) {
         
     </form>
                     
-
-    <!-- Logout Modal-->
     <!-- <div class="modal fade" id="gerenciarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
