e2c6ca4f9ed8aca8374b46919cb39e2aa756f0ff
clfonte <kamilafonte@gmail.com>
2020-12-09 20:53:44 -0300

    Logout de professor resolvido

--- professor/index.php
+++ professor/index.php
@@ -82,8 +82,9 @@ $base = "$h://$site:$porta/$url"
 
           </li>
           <!-- Botão SAIR -->
+          <!-- Botão SAIR -->
           <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
               <i class="fas fa-sign-out-alt"></i>
             </a>
           </li>
@@ -229,6 +230,25 @@ $base = "$h://$site:$porta/$url"
   <?php
   //}
   ?>
+
+      <!-- Logout Modal-->
+      <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Sair do Sistema?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Selecione SAIR para efetuar o logout do Sistema</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Voltar</button>
+            <a class="btn btn-primary" href="sair.php">Sair</a>
+          </div>
+        </div>
+      </div>
+    </div>
   <!-- REQUIRED SCRIPTS -->
 
   <!-- jQuery -->
