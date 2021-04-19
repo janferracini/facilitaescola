8bafc05cb59be2a214f415253a7bdf3275431758
Jan Ferracini <janferracini@gmail.com>
2021-04-19 15:11:03 -0300

    modificacao arquivo turma -> professor

--- professor/listar/turma.php
+++ professor/listar/turma.php
@@ -3,9 +3,13 @@ if (!isset($_SESSION['facilita_escola']['id'])) {
     exit;
 }
 
-$sql = "SELECT t.*, p.*
+$sql = "SELECT t.*, p.*, tm.*, m.pessoa_id, pe.nome
         FROM turma t
-        INNER JOIN periodo p (p.id = t.periodo_id)";
+        INNER JOIN periodo p ON (p.id = t.periodo_id)
+        INNER JOIN turma_matricula tm ON (tm.turma_id = t.id)
+        INNER JOIN matricula m ON (m.id = tm.matricula_id)
+        INNER JOIN pessoa pe ON (pe.id = m.pessoa_id)
+        WHERE id = $id";
 
 $consulta = $pdo->prepare($sql);
 $consulta->execute();
@@ -23,7 +27,7 @@ while ($dados = $consulta->fetch(PDO::FETCH_OBJ)) {
     <div class="container-fluid">
         <div class="row">
             <div>
-                <!-- <h1 class="m-0 text-dark">Alunos Turma <?php echo "$serie " - " $descricao " - " $periodo" ?></h1> -->
+                <h1 class="m-0 text-dark">Alunos Turma <?php echo "$serie " - " $descricao " - " $periodo" ?></h1>
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
