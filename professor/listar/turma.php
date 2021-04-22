37da61dc89ea68e9f44ddc8e278e85c64ba6d5d9
Jan Ferracini <janferracini@gmail.com>
2021-04-22 15:23:52 -0300

    Listagem de turmas e alunos por turma no usuário PROF

--- professor/listar/turma.php
+++ professor/listar/turma.php
@@ -1,38 +1,82 @@
 <?php
-if (!isset($_SESSION['facilita_escola']['id'])) {
-    exit;
-}
+// if (!isset($_SESSION['facilita_escola']['id'])) {
+//     exit;
+// }
 
-$sql = "SELECT t.*, p.*, tm.*, m.pessoa_id, pe.nome
-        FROM turma t
-        INNER JOIN periodo p ON (p.id = t.periodo_id)
-        INNER JOIN turma_matricula tm ON (tm.turma_id = t.id)
-        INNER JOIN matricula m ON (m.id = tm.matricula_id)
-        INNER JOIN pessoa pe ON (pe.id = m.pessoa_id)
-        WHERE id = $id";
 
-$consulta = $pdo->prepare($sql);
-$consulta->execute();
-
-while ($dados = $consulta->fetch(PDO::FETCH_OBJ)) {
-    $id = $dados->id;
-    $serie = $dados->serie;
-    $descricao = $dados->descricao;
-    $periodo = $dados->periodo;
-    $disciplina = $dados->disciplina;
-} ?>
+?>
 
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
+        <?php
+        $sql = "SELECT  t.id tid, t.*, p.*
+                    FROM turma t
+                    INNER JOIN periodo p ON (t.periodo_id = p.id )
+                    WHERE t.id = :id";
+
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam("id", $id);
+        $consulta->execute();
+
+        $dados = $consulta->fetch(PDO::FETCH_OBJ);
+        if (empty($dados->id)) {
+            echo "<p class='alert alert-danger'> Turma não encontrada</p>";
+            exit;
+        } else {
+
+            $id = $dados->tid;
+            $serie = $dados->serie;
+            $descricao = $dados->descricao;
+            $ano = $dados->ano;
+            $periodo = $dados->periodo;
+        }
+        ?>
         <div class="row">
             <div>
-                <h1 class="m-0 text-dark">Alunos Turma <?php echo "$serie " - " $descricao " - " $periodo" ?></h1>
+                <h1 class="m-0 text-dark">Alunos <?php echo  $serie . " " . $descricao . " / " . $periodo; ?></h1>
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
 <!-- /.content-header -->
 <div class="container">
+    <div class="card-body table-responsive p-0 mt-3">
+        <table id="tabAluno" class="table table-hover text-nowrap">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th width=200px>Matrícula</th>
+                    <th width=200px>Contato</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT t.id tid, t.*, tm.*, m.pessoa_id, m.matricula, pe.nome, pe.telefone1
+                        FROM turma t
+                        INNER JOIN turma_matricula tm ON (tm.turma_id = t.id)
+                        INNER JOIN matricula m ON (m.id = tm.matricula_id)
+                        INNER JOIN pessoa pe ON (pe.id = m.pessoa_id)
+                        WHERE t.id = $id
+                        ORDER BY pe.nome";
+
+                $consulta = $pdo->prepare($sql);
+                $consulta->execute();
+
+                while ($dados = $consulta->fetch(PDO::FETCH_OBJ)) {
+                    $nome = $dados->nome;
+                    $matricula = $dados->matricula;
+                    $telefone = $dados->telefone1;
+
+                    echo '<tr>
+                            <td>' . $nome . '</td>
+                            <td>' . $matricula . '</td>
+                            <td>' . $telefone . '</td>
+                        </tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 </div>
