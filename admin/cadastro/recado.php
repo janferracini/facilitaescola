68f221bb10670d06a2d039d8499b7636de86b5fd
clfonte <kamilafonte@gmail.com>
2021-05-08 20:34:26 -0300

    Merge branch 'master' of https://github.com/janferracini/facilitaescola
    # Conflicts:
    #	admin/cadastro/admin.php

--- admin/cadastro/recado.php
+++ admin/cadastro/recado.php
@@ -35,7 +35,6 @@ if (!empty($id)) {
     $conteudo      = $dados->conteudo;
     $data_postagem = $dados->data_postagem;
     $turma_id      = $dados->turma_id;
-
 }
 ?>
 
@@ -66,44 +65,41 @@ if (!empty($id)) {
 
             <div class="col-sm-12">
                 <label for="titulo"> Título: </label>
-                <input type="text" id="titulo" name="titulo" class="form-control" 
-                required data-parsley-required-message="Preencha o título do recado" placeholder="Informe o título do recado">
+                <input type="text" id="titulo" name="titulo" class="form-control" required data-parsley-required-message="Preencha o título do recado" placeholder="Informe o título do recado">
             </div>
-            
+
             <div class="col-sm-12">
                 <label for="conteudo"> Conteúdo: </label>
-                <textarea type="text" name="conteudo" id="conteudo" class="form-control"
-                required data-parsley-required-message="Informe o conteúdo referente ao recado" placeholder=""></textarea>
+                <textarea type="text" name="conteudo" id="conteudo" class="form-control" required data-parsley-required-message="Informe o conteúdo referente ao recado" placeholder=""></textarea>
             </div>
 
             <!-- envio para qual turma será -->
             <div class="form-group">
-                    <label for="turma">Turma</label>
-                    <input type="text" name="turma_id" id="turma_id" class="form-control" list="listaTurma"
-                    data-parsley-required-message="Selecione a turma" value="<?php
-                                                if (!empty($turma_id)) echo "$turma - $turma_id";
-                                                ?>">
-                    <datalist id="listaTurma">
-                        <?php
-                            $sql = "SELECT id, serie, descricao, ano, periodo_id
+                <label for="turma">Turma</label>
+                <input type="text" name="turma_id" id="turma_id" class="form-control" list="listaTurma" data-parsley-required-message="Selecione a turma" value="<?php
+                                                                                                                                                                    if (!empty($grade_id)) echo "$turma - $grade_id";
+                                                                                                                                                                    ?>">
+                <datalist id="listaTurma">
+                    <?php
+                    $sql = "SELECT id, serie, descricao, ano, periodo_id
                                     FROM turma
                                     ORDER BY id";
-                            $consulta = $pdo->prepare($sql);
-                            $consulta->execute();
-                            
-                            while ($d = $consulta->fetch(PDO::FETCH_OBJ)) {
-                                //separar os dados
-                                $turma_id    = $dados->id;
-                                $serie       = $dados->serie;
-                                $descricao   = $dados->descricao;
-                                $ano         = $dados->ano;
-                                $periodo_id  = $dados->periodo_id;
+                    $consulta = $pdo->prepare($sql);
+                    $consulta->execute();
 
-                                echo '<option value=" ' . $turma . ' - ' . $turma_id . ' - ' . $serie . ' - ' . $descricao . ' - ' . $ano . ' - ' . $periodo_id . '">';
-                            };
-                        ?>
-                    </datalist>
-                </div>
+                    while ($d = $consulta->fetch(PDO::FETCH_OBJ)) {
+                        //separar os dados
+                        $turma_id    = $dados->id;
+                        $serie       = $dados->serie;
+                        $descricao   = $dados->descricao;
+                        $ano         = $dados->ano;
+                        $periodo_id  = $dados->periodo_id;
+
+                        echo '<option value=" ' . $turma . ' - ' . $turma_id . ' - ' . $serie . ' - ' . $descricao . ' - ' . $ano . ' - ' . $periodo_id . '">';
+                    };
+                    ?>
+                </datalist>
+            </div>
 
         </div>
 
