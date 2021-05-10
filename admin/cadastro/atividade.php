06958e634c5ca172ab4ec724a7b380cde58b2ed5
clfonte <kamilafonte@gmail.com>
2021-05-09 23:33:44 -0300

    Datalist de turmas ok, arquivo nao salvando no momento do envio

--- admin/cadastro/atividade.php
+++ admin/cadastro/atividade.php
@@ -7,16 +7,23 @@
 if (!isset($id)) $id = "";
 
 // tabela arquivo
-$descricao = $arquivo = $data_postagem = $grade_id  = '';
+$descricao = $arquivo = $data_postagem = $grade_id  = "";
+// tabela grade
+$turma_id = "";
 // tabela turma
-$serie = $descricao = $ano = $periodo_id = '';
+$serie = $descricao = $ano = $periodo_id = "";
+// tabela periodo
+$periodo = "";
 
 if (!empty($id)) {
     //select nos dados do cliente
-    $sql = "SELECT  *
-            FROM arquivo a
-            INNER JOIN grade g ON (g.id = a.grade_id)
-            WHERE p.id = :id
+    $sql = "SELECT a.id aid, a.*, g.id gid, g.turma_id tid, t.*, 
+                p.id pid, p.*
+            FROM atividade a
+            INNER JOIN grade g ON (g.id = a.grade_id) 
+            INNER JOIN turma t ON (t.id = g.turma_id) 
+            INNER JOIN periodo p ON (p.id = t.periodo_id)
+            WHERE a.id = :id
             LIMIT 1";
 
     $consulta = $pdo->prepare($sql);
@@ -29,12 +36,19 @@ if (!empty($id)) {
         echo "<p class='alert alert-danger'>Não há ativadades a serem listadas.</p>";
     }
 
-    $id            = $dados->id;
-    $descricao     = $dados->descricao;
-    $arquivo       = $dados->arquivo;
+    $id         = $dados->aid;
+    $descricao  = $dados->descricao;
+    $arquivo    = $dados->arquivo;
+    // tabela grade
+    $grade_id   = $dados->gid;
+    // tabela periodo
+    $periodo    = $dados->periodo;
+    $periodo_id = $dados->pid;
+    // tabela turma
+    $turma_id  = $dados->tid;
+    $serie     = $dados->serie;
+    $ano       = $dados->ano;
     $data_postagem = $dados->data_postagem;
-    $grade_id      = $dados->grade_id;
-
 }
 ?>
 
@@ -63,31 +77,51 @@ if (!empty($id)) {
 
         <div class="row mb-3">
 
-            <div class="col-sm-12">
-                <label for="grade_id"> Selecione a turma: </label>
-                <input type="text" id="turma" name="turma" class="form-control" 
-                required data-parsley-required-message="Selecione uma turma" placeholder="Selecione a disciplina desejada">
+            <!-- selecionar a turma -->
+            <div class="col-12 ">
+                <label for="turma">Turma</label>
+                <input type="hidden" class="form-control" name="tid" id="tid" readonly value="<?= $turma_id ?>">
+                <input id="grade_id" name="grade_id" class="form-control" list="listaTurma" data-parsley-required-message="Selecione a turma" value="<?php if (!empty($id)) echo "$turma_id - $serie $descricao / $periodo ($ano)"; ?>">
+                <datalist id="listaTurma">
+                    <?php
+                    $sql = "SELECT t.*,t.id tid, p.*
+                                        FROM turma t
+                                        INNER JOIN periodo p ON (p.id = t.periodo_id)
+                                        ORDER BY serie";
+                    $consulta = $pdo->prepare($sql);
+                    $consulta->execute();
+
+                    while ($dados = $consulta->fetch(PDO::FETCH_OBJ)) {
+                        // separar os dados
+                        $serie     = $dados->serie;
+                        $descricao = $dados->descricao;
+                        $ano       = $dados->ano;
+                        $periodo   = $dados->periodo;
+                        $turma_id  = $dados->tid;
+                        echo '<option value="' . $serie . ' ' . $descricao . ' / ' . $periodo . ' (' . $ano . ')">';
+                    };
+                    ?>
+                </datalist>
             </div>
 
             <div class="col-sm-12">
                 <label for="atividade">Atividade:</label>
-                <textarea type="text" name="atividade" id="atividade" class="form-control" 
-                    required data-parsley-required-message="Preencha a descrição da atividade" value="<?= $atividade ?>" placeholder="Informe a descrição da atividade">
-                </textarea>
+                <textarea type="text" name="atividade" id="atividade" class="form-control" required data-parsley-required-message="Preencha a descrição da atividade" value="<?= $atividade ?>" placeholder="Informe a descrição da atividade"></textarea>
             </div>
-            
+
             <div class="col-sm-12">
                 <label for="arquivo">Arquivo:</label>
-                <input type="file" name="arquivo" id="arquivo" class="form-control" accept=".jpg, .jpeg, .docx, .pdf"
-                required data-parsley-required-message="Selecione o arquivo" 
-                value="<?= $arquivo ?>" placeholder="Selecione o arquivo a atividade">
+                <input type="file" name="arquivo" id="arquivo" class="form-control" accept=".jpg, .jpeg, .docx, .pdf" required data-parsley-required-message="Selecione o arquivo" value="<?= $arquivo ?>" placeholder="Selecione o arquivo da atividade">
             </div>
 
         </div>
 
-        <button type="submit" class="btn btn-success margin">
-            <i class="fas fa-check"></i> Gravar Dados
-        </button>
+        <div class="float-right">
+            <button type="submit" class="btn btn-outline-laranja margin">
+                <i class="fas fa-check"></i> Gravar Dados
+            </button>
+        </div>
+        <div class="clearfix"></div> <!-- Ignora os floats -->
     </form>
 
 </div>
