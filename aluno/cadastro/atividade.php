25f517cc5395ec524f46f2dc3816dce18b7d2602
Jan Ferracini <janferracini@gmail.com>
2021-05-16 04:00:41 -0300

    listar ATIVIDADE aluno

---
+++ aluno/cadastro/atividade.php
@@ -0,0 +1,143 @@
+<?php
+if (!isset($_SESSION["hqs"]["id"]) && (($_SESSION["facilita_escola"]["tipo_cadastro"] != 2))) {
+    echo "<script>alert('Erro na requisição da página');location.href='javascript:history.back()'</script>";
+    exit;
+}
+
+if (!isset($id)) $id = "";
+
+// tabela arquivo
+$descricao = $arquivo = $data_postagem = $grade_id  = "";
+// tabela grade
+$turma_id = "";
+// tabela turma
+$serie = $descricao = $ano = $periodo_id = "";
+// tabela periodo
+$periodo = "";
+
+if (!empty($id)) {
+    //select nos dados do cliente
+    $sql = "SELECT a.id aid, a.*, g.id gid, g.turma_id tid, t.*, 
+                p.id pid, p.*, d.*, pr.*, pe.id, pe.nome
+            FROM atividade a
+            INNER JOIN grade g ON (g.id = a.grade_id) 
+            INNER JOIN disciplina d ON (d.id = g.disciplina_id)
+            INNER JOIN turma t ON (t.id = g.turma_id) 
+            INNER JOIN periodo p ON (p.id = t.periodo_id)
+            INNER JOIN professor pr ON (pr.id = g.professor_id)
+            INNER JOIN pessoa pe ON (pe.id = pr.pessoa_id)
+            WHERE a.id = :id
+            LIMIT 1";
+
+    $consulta = $pdo->prepare($sql);
+    $consulta->bindParam(":id", $id);
+    $consulta->execute();
+
+    $dados = $consulta->fetch(PDO::FETCH_OBJ);
+
+    if (empty($dados->id)) {
+        echo "<p class='alert alert-danger'>Não há ativadades a serem listadas.</p>";
+    }
+
+    $id         = $dados->aid;
+    $atividade  = $dados->atividade;
+    $arquivo    = $dados->arquivo;
+    $grade_id   = $dados->gid;
+    $periodo    = $dados->periodo;
+    $periodo_id = $dados->pid;
+    $turma_id  = $dados->tid;
+    $ano       = $dados->ano;
+    $data_postagem = $dados->data_postagem;
+    $disciplina = $dados->disciplina;
+    $serie = $dados->serie;
+    $professor = $dados->nome;
+    $descricao = $dados->descricao;
+}
+?>
+
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div>
+                <h1 class="m-0 text-dark">Cadastro de Atividade</h1>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<div class="container">
+    <div class="float-right">
+        <a href="listar/atividade" class="btn btn-outline-info">Listar Atividades</a>
+    </div>
+    <div class="clearfix"></div> <!-- Ignora os floats -->
+    <form action="salvar/atividade" name="formCadastro" method="post" data-parsley-validate enctype="multipart/form-data" role="form">
+        <div class="row mb-3">
+            <!-- selecionar a turma -->
+            <div class="col-12 ">
+                <label for="turma">Grade</label>
+                <input type="hidden" class="form-control" name="id" id="id" readonly value="<?= $id ?>">
+                <input id="grade_id" name="grade_id" class="form-control" list="listaTurma" data-parsley-required-message="Selecione a grade" value="<?php if (!empty($id)) echo "$grade_id - $disciplina - $serie $descricao / $periodo ($professor)"; ?>">
+                <datalist id="listaTurma">
+                    <?php
+                    $sql = "SELECT  g.id idgrade, g.*, t.*, pd.*, d.*, p.id, pe.id , pe.nome
+                            FROM grade g
+                            INNER JOIN turma t ON (t.id = g.turma_id)
+                            INNER JOIN periodo pd ON (pd.id = t.periodo_id)
+                            INNER JOIN disciplina d ON (d.id = g.disciplina_id)
+                            INNER JOIN professor p ON (p.id = g.professor_id)
+                            INNER JOIN pessoa pe ON (pe.id = p.pessoa_id)
+                            ORDER BY t.descricao";
+
+                    $consulta = $pdo->prepare($sql);
+                    $consulta->execute();
+
+                    while ($dados = $consulta->fetch(PDO::FETCH_OBJ)) {
+
+                        $grade_id = $dados->idgrade;
+                        $serie = $dados->serie;
+                        $descricao = $dados->descricao;
+                        $periodo = $dados->periodo;
+                        $disciplina = $dados->disciplina;
+                        $professor = $dados->nome;
+                        echo '<option value="' . $grade_id . ' - ' . $disciplina . ' - ' . $serie . ' ' . $descricao . ' / ' . $periodo . ' (' . $professor . ')">';
+                    };
+                    ?>
+                </datalist>
+            </div>
+            <div class="col-sm-12">
+                <label for="atividade">Atividade:</label>
+                <textarea type="text" name="atividade" id="atividade" class="form-control" required data-parsley-required-message="Preencha a descrição da atividade" placeholder="Informe a descrição da atividade"><?php if (!empty($id)) echo $atividade ?></textarea>
+            </div>
+            <div class="col-sm-12">
+                <?php
+                $r = 'required data-parsley-required-message="Selecione uma foto"';
+                //vai mostrar que o campo é requirido por padrão, a não ser que seja uma edição 
+                //se não tiver vazio o ID, quer dizer que é inserção e não aparece o required
+                if (!empty($id)) $r = '';
+                ?>
+                <label for="arquivo">Arquivo: </label>
+                <spam>(Tipos permitidos: .jpg, .jpeg, .docx, .doc ou .pdf, tamanho máximo: 3Mb)</spam>
+                <!-- guarda o nome do arquivo para quando editar -->
+                <input type="hidden" name="capa" value="<?= $arquivo; ?>">
+                <input type="file" name="arquivo" id="arquivo" class="form-control" accept=".jpg, .jpeg, .docx, .pdf, .doc, .odt" <?= $r; ?> placeholder="Selecione o arquivo da atividade">
+            </div>
+
+            <?php
+            if (!empty($id)) {
+
+                echo "<div class='col-sm-12 mt-2'>
+                <p>Arquivo já encaminhado: <a href='../atividades/$arquivo' download='Atividade $disciplina-$serie $descricao($periodo)'>Download</a></p>
+                </div>";
+            }
+            ?>
+        </div>
+        <div class="float-right">
+            <button type="submit" class="btn btn-outline-laranja margin">
+                <i class="fas fa-check"></i> Gravar Dados
+            </button>
+        </div>
+        <div class="clearfix"></div> <!-- Ignora os floats -->
+    </form>
+</div>
---
+++ aluno/listar/atividade.php
@@ -0,0 +1,92 @@
+<?php
+//verificar se está logado
+if (!isset($_SESSION["hqs"]["id"]) && (($_SESSION["facilita_escola"]["tipo_cadastro"] != 2))) {
+    echo "<script>alert('Erro na requisição da página');location.href='javascript:history.back()'</script>";
+    exit;
+}
+
+$idaluno = $_SESSION['facilita_escola']['id'];
+?>
+
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div>
+                <h1 class="m-0 text-dark">Atividades Cadastradas</h1>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<div class="container">
+    <div class="card-body table-responsive p-0 mt-3">
+        <table id="tabDisciplina" class="table table-hover text-nowrap">
+            <thead>
+                <tr>
+                    <th style="width: 20%;">Data de Postagem</th>
+                    <th style="width: 30%;">Atividade</th>
+                    <th style="width: 30%;">Disciplina</th>
+                    <th style="width: 20%;">Salvar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT a.id aid, a.atividade, a.arquivo, a.grade_id, date_format(a.data_postagem, '%d/%m/%Y') data_postagem,
+                                g.*, t.*, p.*, d.*, t.*, tm.*, m.*, pe.id
+                        FROM atividade a
+                        INNER JOIN grade g ON ( a.grade_id = g.id) 
+                        INNER JOIN disciplina d ON (d.id = g.disciplina_id)
+                        INNER JOIN turma t ON (t.id = g.turma_id) 
+                        INNER JOIN periodo p ON (p.id = t.periodo_id)
+                        INNER JOIN turma_matricula tm ON (tm.turma_id = t.id)
+                        INNER JOIN matricula m ON (tm.matricula_id)
+                        INNER JOIN pessoa pe ON (pe.id = m.pessoa_id)
+                        WHERE pe.id = $idaluno
+                        GROUP BY aid DESC";
+
+                $consulta = $pdo->prepare($sql);
+                $consulta->execute();
+
+                while ($dados = $consulta->fetch(PDO::FETCH_OBJ)) {
+                    //atividade
+                    $id         = $dados->aid;
+                    $atividade  = $dados->atividade;
+                    $arquivo    = $dados->arquivo;
+                    $data       = $dados->data_postagem;
+                    //discilina
+                    $disciplina = $dados->disciplina;
+                    //turma
+                    $serie      = $dados->serie;
+                    $descricao  = $dados->descricao;
+                    //periodo
+                    $periodo    = $dados->periodo;
+
+                    echo '<tr>
+                <td>' . $data . '</td>
+                <td>' . $atividade . '</td>
+                <td>' . $disciplina . ' </td>
+                <td>
+                <a href="../atividades/' . $arquivo . '" download="Atividade de ' . $disciplina . '(' . $data . ')" class="btn btn-success btn-sm">
+                        <i class="fas fa-file-download"></i>
+                    </a>
+                </td>
+                </tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- /.card-body -->
+</div>
+
+<script>
+    //função para perguntar se deseja excluir. Se sim, direcionar para o endereço de exclusão
+    function excluir(id) {
+        //perguntar
+        if (confirm("Deseja mesmo excluir?")) {
+            //direcionar para exclusão
+            location.href = "excluir/atividade/" + id;
+        }
+    }
+</script>
