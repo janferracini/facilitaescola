322529d34aa7fc60a9db9193240d2818402dd854
Jan Ferracini <janferracini@gmail.com>
2021-05-24 00:42:17 -0300

    apagar arquivo da pasta ao deletar atividade

--- admin/excluir/atividade.php
+++ admin/excluir/atividade.php
@@ -8,6 +8,15 @@ if ($_SESSION["facilita_escola"]["tipo_cadastro"] != 1) {
     echo "<script>alert('Erro na requisição da página, faça login novamente para continuar');location.href='sair.php'</script>";
     exit;
 }
+include('functions.php');
+
+$sql = "SELECT arquivo FROM atividade WHERE id = $id";
+$consulta = $pdo->prepare($sql);
+$consulta->bindParam(":id", $id);
+$consulta->execute();
+$dados = $consulta->fetch(PDO::FETCH_OBJ);
+
+$arquivo = $dados->arquivo;
 
 //verificar se id está vazia
 if (empty($id)) {
@@ -20,13 +29,10 @@ if (empty($id)) {
 $sql = "DELETE FROM atividade WHERE id = ? limit 1";
 $consulta = $pdo->prepare($sql);
 $consulta->bindParam(1, $id);
-//verificar se não executou
+
 if (!$consulta->execute()) {
-
-    //consulta erros - sempre depois do execute
-    //echo $consulta->errorInfo()[2];
-
     echo "<script> alert('Erro ao excluir');javascript:history.back();</script>";
 }
 
-echo "<script>alert('Registro excluído');location.href='listar/atividade'</script>";
+echo "<script>alert('Registro excluído')'</script>";
+apagaImagem($arquivo);
