52aabfca540602872beda89442bb552021504f90
clfonte <kamilafonte@gmail.com>
2021-07-15 21:18:59 -0300

    Arquivos avulsos

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ error_reporting(E_ALL);
 
 
 //iniciar a variavel pagina
-//$pagina = "paginas/login";
 
 include "config/conexao.php";
 
@@ -16,9 +15,7 @@ $site   = $_SERVER['SERVER_NAME'];
 $porta  = $_SERVER["SERVER_PORT"];
 $url    = $_SERVER['SCRIPT_NAME'];
 $h      = $_SERVER['REQUEST_SCHEME'];
-//
 $base = "$h://$site:$porta/$url"
-//
 
 ?>
 
@@ -28,12 +25,12 @@ $base = "$h://$site:$porta/$url"
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
   <title>Facilita Escola | Log in</title>
+
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <link rel="shortcut icon" href="docs/assets/img/FE-icone.ico">
-
   <!-- Font Awesome -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
@@ -42,27 +39,24 @@ $base = "$h://$site:$porta/$url"
   <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.css">
-
   <link rel="stylesheet" href="dist/css/style.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
 <body class="hold-transition login-page">
+
   <div class="login-box">
-  <?php 
+    <?php
     include "paginas/login.php";
+    ?>
 
-  ?>
-    
-
-  <!-- jQuery -->
-  <script src="../../plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="../../dist/js/adminlte.min.js"></script>
-
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
